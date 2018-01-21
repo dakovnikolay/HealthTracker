4814f764ca332bb1efc794a775df90e4e1fe5e93
Nikolay Dakov <ndakov@Nikolays-Mac.local>
2018-01-21 13:25:26 -0800

    Initial Commit

---
+++ HealthTracker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HealthTracker
+//
+//  Created by Nikolay Dakov on 21/01/2018.
+//  Copyright © 2018 Nikolay Dakov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

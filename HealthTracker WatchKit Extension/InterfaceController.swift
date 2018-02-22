91ea4017ff8803106f705694f34d47045b6e2daf
Nikolay Dakov <ndakov@Nikolays-Mac.local>
2018-02-21 18:08:43 -0800

    Initial commit.

--- HealthTracker WatchKit Extension/InterfaceController.swift
+++ HealthTracker WatchKit Extension/InterfaceController.swift
@@ -6,231 +6,141 @@
 //  Copyright © 2018 Nikolay Dakov. All rights reserved.
 //
 
-import WatchKit
-import HealthKit
-import Foundation
 
+
+import Foundation
+import HealthKit
+import WatchKit
+import CoreBluetooth
 
 class InterfaceController: WKInterfaceController, HKWorkoutSessionDelegate {
     
+    @IBOutlet fileprivate weak var label: WKInterfaceLabel!
+    @IBOutlet fileprivate weak var startStopButton : WKInterfaceButton!
+    
     let healthStore = HKHealthStore()
     
-    // Used to track the current `HKWorkoutSession`.
-    var currentWorkoutSession: HKWorkoutSession?
+    //State of the app - is the workout activated
+    var workoutActive = false
     
-    var workoutBeginDate: Date?
-    var workoutEndDate: Date?
+    // define the activity type and location
+    var workoutSession : HKWorkoutSession?
+    let heartRateUnit = HKUnit(from: "count/min")
+    var anchor = HKQueryAnchor(fromValue: Int(HKAnchoredObjectQueryNoAnchor))
     
-    var isWorkoutRunning = false
-    
-    var currentQuery: HKQuery?
-    
-    var activeEnergySamples = [HKQuantitySample]()
-    
-    // Start with a zero quantity.
-    var currentActiveEnergyQuantity = HKQuantity(unit: HKUnit.kilocalorie(), doubleValue: 0.0)
-    
-    @IBOutlet var workoutButton: WKInterfaceButton!
-    @IBOutlet var activeEnergyBurnedLabel: WKInterfaceLabel!
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+    }
     
     override func willActivate() {
-        // This method is called when watch view controller is about to be visible to user.
         super.willActivate()
         
-        // Only proceed if health data is available.
-        guard HKHealthStore.isHealthDataAvailable() else { return }
+        guard HKHealthStore.isHealthDataAvailable() == true else {
+            label.setText("not available")
+            return
+        }
         
-        // We need to be able to write workouts, so they display as a standalone workout in the Activity app on iPhone.
-        // We also need to be able to write Active Energy Burned to write samples to HealthKit to later associating with our app.
-        let typesToShare = Set([
-            HKObjectType.workoutType(),
-            HKObjectType.quantityType(forIdentifier: HKQuantityTypeIdentifier.activeEnergyBurned)!])
+        guard let quantityType = HKQuantityType.quantityType(forIdentifier: HKQuantityTypeIdentifier.heartRate) else {
+            displayNotAllowed()
+            return
+        }
         
-        let typesToRead = Set([
-            HKObjectType.quantityType(forIdentifier: HKQuantityTypeIdentifier.activeEnergyBurned)!])
-        
-        healthStore.requestAuthorization(toShare: typesToShare, read: typesToRead) { success, error in
-            if let error = error, !success {
-                print("You didn't allow HealthKit to access these read/write data types. In your app, try to handle this error gracefully when a user decides not to provide access. The error was: \(error.localizedDescription). If you're using a simulator, try it on a device.")
+        let dataTypes = Set(arrayLiteral: quantityType)
+        healthStore.requestAuthorization(toShare: nil, read: dataTypes) { (success, error) -> Void in
+            if success == false {
+                self.displayNotAllowed()
             }
         }
     }
     
-    // This will toggle beginning and ending a workout session.
-    @IBAction func toggleWorkout() {
-        if isWorkoutRunning {
-            guard let workoutSession = currentWorkoutSession else { return }
-            
-            healthStore.end(workoutSession)
-            isWorkoutRunning = false
-        } else {
-            // Begin workout.
-            isWorkoutRunning = true
-            
-            // Clear the local Active Energy Burned quantity when beginning a workout session.
-            currentActiveEnergyQuantity = HKQuantity(unit: HKUnit.kilocalorie(), doubleValue: 0.0)
-            
-            currentQuery = nil
-            activeEnergySamples = []
-            
-            // An indoor walk workout session. There are other activity and location types available to you.
-            let workoutSession = HKWorkoutSession(activityType: .walking, locationType: .indoor)
-            workoutSession.delegate = self
-            
-            currentWorkoutSession = workoutSession
-            
-            healthStore.start(workoutSession)
-        }
-    }
-    
-    // MARK: Convenience
-    
-    /*
-     Create and save an HKWorkout with the amount of Active Energy Burned we accumulated during the HKWorkoutSession.
-     
-     Additionally, associate the Active Energy Burned samples to our workout to facilitate showing our app as credited for these samples in the Move graph in the Activity app on iPhone.
-     */
-    func saveWorkout() {
-        // Obtain the `HKObjectType` for active energy burned.
-        guard let activeEnergyType = HKObjectType.quantityType(forIdentifier: HKQuantityTypeIdentifier.activeEnergyBurned) else { return }
-        
-        // Only proceed if both `beginDate` and `endDate` are non-nil.
-        guard let beginDate = workoutBeginDate, let endDate = workoutEndDate else { return }
-        
-        /*
-         NOTE: There is a known bug where activityType property of HKWorkoutSession returns 0, as of iOS 9.1 and watchOS 2.0.1. So, rather than set it using the value from the `HKWorkoutSession`, set it explicitly for the HKWorkout object.
-         */
-        let workout = HKWorkout(activityType: HKWorkoutActivityType.walking, start: beginDate, end: endDate, duration: endDate.timeIntervalSince(beginDate), totalEnergyBurned: currentActiveEnergyQuantity, totalDistance: HKQuantity(unit: HKUnit.meter(), doubleValue: 0.0), metadata: nil)
-        
-        // Save the array of samples that produces the energy burned total
-        let finalActiveEnergySamples = activeEnergySamples
-        
-        guard healthStore.authorizationStatus(for: activeEnergyType) == .sharingAuthorized && healthStore.authorizationStatus(for: HKObjectType.workoutType()) == .sharingAuthorized else { return }
-        
-        healthStore.save(workout) { [unowned self] success, error in
-            if let error = error, !success {
-                print("An error occurred saving the workout. The error was: \(error.localizedDescription)")
-                return
-            }
-            
-            // Since HealthKit completion blocks may come back on a background queue, please dispatch back to the main queue.
-            if success && finalActiveEnergySamples.count > 0 {
-                // Associate the accumulated samples with the workout.
-                self.healthStore.add(finalActiveEnergySamples, to: workout) { success, error in
-                    if let error = error, !success {
-                        print("An error occurred adding samples to the workout. The error was: \(error.localizedDescription)")
-                    }
-                }
-            }
-        }
-    }
-    
-    func beginWorkout(on beginDate: Date) {
-        // Obtain the `HKObjectType` for active energy burned and the `HKUnit` for kilocalories.
-        guard let activeEnergyType = HKObjectType.quantityType(forIdentifier: HKQuantityTypeIdentifier.activeEnergyBurned) else { return }
-        let energyUnit = HKUnit.kilocalorie()
-        
-        // Update properties.
-        workoutBeginDate = beginDate
-        workoutButton.setTitle("End Workout")
-        
-        // Set up a predicate to obtain only samples from the local device starting from `beginDate`.
-        let datePredicate = HKQuery.predicateForSamples(withStart: beginDate, end: nil, options: HKQueryOptions())
-        let devicePredicate = HKQuery.predicateForObjects(from: [HKDevice.local()])
-        let predicate = NSCompoundPredicate(andPredicateWithSubpredicates:[datePredicate, devicePredicate])
-        
-        /*
-         Create a results handler to recreate the samples generated by a query of active energy samples so that they can be associated with this app in the move graph. It should be noted that if your app has different heuristics for active energy burned you can generate your own quantities rather than rely on those from the watch. The sum of your sample's quantity values should equal the energy burned value provided for the workout.
-         */
-        let sampleHandler = { [unowned self] (samples: [HKQuantitySample]) -> Void in
-            DispatchQueue.main.async { [unowned self] in
-                
-                let initialActiveEnergy = self.currentActiveEnergyQuantity.doubleValue(for: energyUnit)
-                
-                let processedResults: (Double, [HKQuantitySample]) = samples.reduce((initialActiveEnergy, [])) { current, sample in
-                    let accumulatedValue = current.0 + sample.quantity.doubleValue(for: energyUnit)
-                    
-                    let ourSample = HKQuantitySample(type: activeEnergyType, quantity: sample.quantity, start: sample.startDate, end: sample.endDate)
-                    
-                    return (accumulatedValue, current.1 + [ourSample])
-                }
-                
-                // Update the UI.
-                self.currentActiveEnergyQuantity = HKQuantity(unit: energyUnit, doubleValue: processedResults.0)
-                self.activeEnergyBurnedLabel.setText("\(processedResults.0)")
-                
-                // Update our samples.
-                self.activeEnergySamples += processedResults.1
-            }
-        }
-        
-        // Create a query to report new Active Energy Burned samples to our app.
-        let activeEnergyQuery = HKAnchoredObjectQuery(type: activeEnergyType, predicate: predicate, anchor: nil, limit: Int(HKObjectQueryNoLimit)) { query, samples, deletedObjects, anchor, error in
-            if let error = error {
-                print("An error occurred with the `activeEnergyQuery`. The error was: \(error.localizedDescription)")
-                return
-            }
-            // NOTE: `deletedObjects` are not considered in the handler as there is no way to delete samples from the watch during a workout.
-            guard let activeEnergySamples = samples as? [HKQuantitySample] else { return }
-            sampleHandler(activeEnergySamples)
-        }
-        
-        // Assign the same handler to process future samples generated while the query is still active.
-        activeEnergyQuery.updateHandler = { query, samples, deletedObjects, anchor, error in
-            if let error = error {
-                print("An error occurred with the `activeEnergyQuery`. The error was: \(error.localizedDescription)")
-                return
-            }
-            // NOTE: `deletedObjects` are not considered in the handler as there is no way to delete samples from the watch during a workout.
-            guard let activeEnergySamples = samples as? [HKQuantitySample] else { return }
-            sampleHandler(activeEnergySamples)
-        }
-        
-        currentQuery = activeEnergyQuery
-        healthStore.execute(activeEnergyQuery)
-    }
-    
-    func endWorkout(on endDate: Date) {
-        workoutEndDate = endDate
-        
-        workoutButton.setTitle("Begin Workout")
-        activeEnergyBurnedLabel.setText("0.0")
-        
-        if let query = currentQuery {
-            healthStore.stop(query)
-        }
-        
-        saveWorkout()
+    func displayNotAllowed() {
+        //label.setText("not allowed")
     }
     
     func workoutSession(_ workoutSession: HKWorkoutSession, didChangeTo toState: HKWorkoutSessionState, from fromState: HKWorkoutSessionState, date: Date) {
-        DispatchQueue.main.async { [unowned self] in
-            switch toState {
-            case .running:
-                self.beginWorkout(on: date)
-                
-            case .ended:
-                self.endWorkout(on: date)
-                
-            default:
-                print("Unexpected workout session state: \(toState)")
-            }
+        switch toState {
+        case .running:
+            workoutDidStart(date)
+        case .ended:
+            workoutDidEnd(date)
+        default:
+            print("Unexpected state \(toState)")
         }
     }
     
     func workoutSession(_ workoutSession: HKWorkoutSession, didFailWithError error: Error) {
-        print("The workout session failed. The error was: \(error.localizedDescription)")
+        // Do nothing for now
+        // NSLog("Workout error: \(error.userInfo)")
     }
     
-    override func awake(withContext context: Any?) {
-        super.awake(withContext: context)
+    func workoutDidStart(_ date : Date) {
+        if let query = createHeartRateStreamingQuery(date) {
+            healthStore.execute(query)
+        } else {
+            label.setText("cannot start")
+        }
+    }
+    
+    func workoutDidEnd(_ date : Date) {
+        if let query = createHeartRateStreamingQuery(date) {
+            healthStore.stop(query)
+            label.setText("---")
+        } else {
+            label.setText("cannot stop")
+        }
+    }
+    
+    // MARK: - Actions
+    @IBAction func startBtnTapped() {
+        if (self.workoutActive) {
+            //finish the current workout
+            self.workoutActive = false
+            self.startStopButton.setTitle("Start")
+            if let workout = self.workoutSession {
+                healthStore.end(workout)
+            }
+        } else {
+            //start a new workout
+            self.workoutActive = true
+            self.startStopButton.setTitle("Stop")
+            startWorkout()
+        }
         
-        // Configure interface objects here.
     }
     
-    override func didDeactivate() {
-        // This method is called when watch view controller is no longer visible
-        super.didDeactivate()
+    func startWorkout() {
+        self.workoutSession = HKWorkoutSession(activityType: HKWorkoutActivityType.crossTraining, locationType: HKWorkoutSessionLocationType.indoor)
+        self.workoutSession?.delegate = self
+        healthStore.start(self.workoutSession!)
     }
     
+    func createHeartRateStreamingQuery(_ workoutStartDate: Date) -> HKQuery? {
+        // adding predicate will not work
+        // let predicate = HKQuery.predicateForSamplesWithStartDate(workoutStartDate, endDate: nil, options: HKQueryOptions.None)
+        
+        guard let quantityType = HKObjectType.quantityType(forIdentifier: HKQuantityTypeIdentifier.heartRate) else { return nil }
+        
+        let heartRateQuery = HKAnchoredObjectQuery(type: quantityType, predicate: nil, anchor: anchor, limit: Int(HKObjectQueryNoLimit)) { (query, sampleObjects, deletedObjects, newAnchor, error) -> Void in
+            guard let newAnchor = newAnchor else {return}
+            self.anchor = newAnchor
+            self.updateHeartRate(sampleObjects)
+        }
+        
+        heartRateQuery.updateHandler = {(query, samples, deleteObjects, newAnchor, error) -> Void in
+            self.anchor = newAnchor!
+            self.updateHeartRate(samples)
+        }
+        return heartRateQuery
+    }
+    
+    func updateHeartRate(_ samples: [HKSample]?) {
+        guard let heartRateSamples = samples as? [HKQuantitySample] else {return}
+        
+        DispatchQueue.main.async {
+            guard let sample = heartRateSamples.first else{return}
+            let value = sample.quantity.doubleValue(for: self.heartRateUnit)
+            self.label.setText(String(UInt16(value)))
+            
+        }
+    }
 }
